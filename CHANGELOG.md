0e0dad453795e090ce6af1b156f6f3b08d85b805
Andrew Kane <acekane1@gmail.com>
2016-10-29 19:09:39 -0700

    Added ability to include manually

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.1.5 [unreleased]
+
+- Added ability to include manually
+
 ## 0.1.4
 
 - Fixed error with `select`
---
+++ lib/str_enum/model.rb
@@ -0,0 +1,31 @@
+require "active_support/concern"
+
+module StrEnum
+  module Model
+    extend ActiveSupport::Concern
+
+    class_methods do
+      def str_enum(column, values, validate: true, scopes: true, accessor_methods: true, prefix: false, suffix: false)
+        values = values.map(&:to_s)
+        validates column, presence: true, inclusion: {in: values} if validate
+        values.each do |value|
+          prefix = column if prefix == true
+          suffix = column if suffix == true
+          method_name = [prefix, value, suffix].select { |v| v }.join("_")
+          scope method_name, -> { where(column => value) } if scopes && !respond_to?(method_name)
+          if accessor_methods && !method_defined?("#{method_name}?")
+            define_method "#{method_name}?" do
+              read_attribute(column) == value
+            end
+          end
+        end
+        after_initialize do
+          send("#{column}=", values.first) unless try(column)
+        end
+        define_singleton_method column.to_s.pluralize do
+          values
+        end
+      end
+    end
+  end
+end
