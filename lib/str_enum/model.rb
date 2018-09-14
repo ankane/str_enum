566ea1a1234e771f3d055134a253d8d5112d885b
Andrew <acekane1@gmail.com>
2018-09-14 16:23:11 +0200

    Use allow_nil instead of allow_blank

--- lib/str_enum/model.rb
+++ lib/str_enum/model.rb
@@ -5,12 +5,12 @@ module StrEnum
     extend ActiveSupport::Concern
 
     class_methods do
-      def str_enum(column, values, validate: true, scopes: true, accessor_methods: true, prefix: false, suffix: false, default: true, allow_blank: false)
+      def str_enum(column, values, validate: true, scopes: true, accessor_methods: true, prefix: false, suffix: false, default: true, allow_nil: false)
         values = values.map(&:to_s)
         if validate
           validate_options = {inclusion: {in: values}}
-          if allow_blank
-            validate_options[:allow_blank] = true
+          if allow_nil
+            validate_options[:allow_nil] = true
           else
             validate_options[:presence] = true
           end
