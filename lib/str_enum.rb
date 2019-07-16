99472003f20322677942a4b04738a93e8163087d
Andrew Kane <andrew@chartkick.com>
2019-07-16 02:41:41 -0400

    Cleaner code [skip ci]

--- lib/str_enum.rb
+++ lib/str_enum.rb
@@ -1,6 +1,9 @@
-require "str_enum/version"
+# dependencies
 require "active_support"
+
+# modules
 require "str_enum/model"
+require "str_enum/version"
 
 ActiveSupport.on_load(:active_record) do
   include StrEnum::Model
