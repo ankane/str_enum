d8492e6a9c7fd77ab01e29f01816c4b158a3ae04
Emil Ong <emil@binti.com>
2020-11-13 10:29:22 -0800

    Allow enum name to differ from column name

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -16,10 +16,12 @@ ActiveRecord::Migration.create_table :users do |t|
   t.string :status
   t.string :address_status
   t.string :kind
+  t.string :rank_str
 end
 
 class User < ActiveRecord::Base
   str_enum :status, [:active, :archived]
   str_enum :address_status, [:active, :archived], prefix: :address
   str_enum :kind, [:guest, :vip], suffix: true
+  str_enum :rank, [:lowly, :middling, :high_falutin], column: :rank_str
 end
